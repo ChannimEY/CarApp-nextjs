11ef346ac5adc879ce5eafd69ec7dd17ff77c4aa
ChannimEY <channimey1@gmail.com>
2025-07-10 18:34:16 +0700

    add metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,7 @@
-import type { Metadata } from "next";
 import { CarouselDApiDemo } from "@/components/(landing)/carousel/CarouselComponent";
 import { MarqueeDemo } from "@/components/(landing)/testimonial/TestimonialComponent";
 import { HeroVideoDialogDemo } from "@/components/(landing)/VideoComponent/VideoComponent";
 
-
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "Home",
-    description: "Welcome to the best car selling platform!",
-    openGraph: {
-      title: "Home | Car Selling",
-      description: "Explore top cars and services on our homepage.",
-      images: [
-        {
-          url: "https://northfleet.in/wp-content/uploads/2024/10/Types-of-Cars-in-India.webp",
-          width: 800,
-          height: 600,
-          alt: "Car selling platform",
-        }
-      ],
-    },
-  };
-}
 
 export default function Home() {
   return (
