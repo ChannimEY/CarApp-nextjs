614903b75612f827a22a787f991ba9cc3a2e19d2
ChannimEY <channimey1@gmail.com>
2025-07-10 22:09:58 +0700

    add metadata

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -5,27 +5,28 @@ import type { Metadata } from "next";
 
 import StyledComponentsRegistry from "@/lib/registry"
 import BlogListSkeleton from "@/components/Skeleton/BlogSkeleton"
-export const metadata: Metadata = {
 
-   title:{
-      template:'%s | Car Selling',
-      default: "BlogPage"
-
-   },
-  openGraph:{
-    title:'Car Selling',
-    description:'This is blog page of car selling',
-    images:[
-     {
-      url:'https://leasecar.uk/images/main/cars/2/2/102123/corsa-front.jpeg',
-      width: 800,
-      height: 600,
-      alt: "Car selling platform",
-      type: 'image/png'
-     }
-    ]
-  }
-};
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: {
+      template: '%s | Car Selling',
+      default: 'Product Page',
+    },
+    openGraph: {
+      title: 'Products page',
+      description: 'This is the Product page',
+      images: [
+        {
+          url: 'https://www.autoshippers.co.uk/blog/wp-content/uploads/bugatti-centodieci.jpg',
+          width: 800,
+          height: 650,
+          alt: 'Car',
+          type: 'image/png',
+        },
+      ],
+    },
+  };
+}
 
 export default function BlogLayout(
     {children}:{children: React.ReactNode}
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -4,26 +4,27 @@ import FetchCar from '@/lib/api'
 import { CarData } from '@/lib/car-type'
 import React from 'react'
 import { Metadata } from "next";
-export const metadata: Metadata = {
-
-   title:{
-      template:'%s | Car Selling',
-      default: "Product Page"
-   },
-  openGraph:{
-    title:'Products page',
-    description:'THis is Product page',
-    images:[
-     {
-      url:'https://leasecar.uk/images/main/cars/2/2/102123/corsa-front.jpeg',
-      width: 800,
-      height: 650,
-      alt: "Car",
-      type: 'image/png'
-     }
-    ]
-  }
-};
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: {
+      template: '%s | Car Selling',
+      default: 'Product Page',
+    },
+    openGraph: {
+      title: 'Products page',
+      description: 'This is the Product page',
+      images: [
+        {
+          url: 'https://leasecar.uk/images/main/cars/2/2/102123/corsa-front.jpeg',
+          width: 800,
+          height: 650,
+          alt: 'Car',
+          type: 'image/png',
+        },
+      ],
+    },
+  };
+}
 const ProductPage = async () => {
   const  data:CarData[] = await FetchCar(0,5)
   console.log(data)
