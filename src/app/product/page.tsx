9fdac294db9bca8bace23c72b8e28ff19b085907
ChannimEY <channimey1@gmail.com>
2025-07-10 18:37:55 +0700

    add metadata

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,44 +1,38 @@
 
-import type { Metadata } from "next";
-import React from "react";
-import DisplayProductComponent from "@/components/products/DisplayProductComponent";
-import FetchCar from "@/lib/api";
-import { CarData } from "@/lib/car-type";
+import DisplayProductComponent from '@/components/products/DisplayProductComponent'
+import FetchCar from '@/lib/api'
+import { CarData } from '@/lib/car-type'
+import React from 'react'
+import { Metadata } from "next";
+export const metadata: Metadata = {
 
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "Product Page",
-    openGraph: {
-      title: "Car Selling - Product Page",
-      description: "This is product page of car selling",
-      images: [
-        {
-          url: 'https://leasecar.uk/images/main/cars/2/2/102123/corsa-front.jpeg',
-          width: 800,
-          height: 600,
-          alt: "Car selling platform",
-          type: 'image/png'
-        }
-      ]
-    }
-  };
-}
-
+   title:{
+      template:'%s | Car Selling',
+      default: "Product Page"
+   },
+  openGraph:{
+    title:'Products page',
+    description:'THis is Product page',
+    images:[
+     {
+      url:'https://leasecar.uk/images/main/cars/2/2/102123/corsa-front.jpeg',
+      width: 800,
+      height: 650,
+      alt: "Car",
+      type: 'image/png'
+     }
+    ]
+  }
+};
 const ProductPage = async () => {
-  const data: CarData[] = await FetchCar(0, 5);
-
+  const  data:CarData[] = await FetchCar(0,5)
+  console.log(data)
   return (
     <div>
-      <DisplayProductComponent
-        tagline={'Latest Updates'}
-        heading={'New Coming'}
-        description={'Discover the latest trends, tips, and best practices in modern web development. From UI components to design systems, stay updated with our expert insights.'}
-        buttonText={'View all cars'}
-        buttonUrl={''}
-        posts={data}
-      />
+      {/* <SWRComponent/> */}
+      <DisplayProductComponent tagline={'Latest Updates'} heading={'New Comming'} description={'Discover the latest trends, tips, and best practices in modern web development. From UI components to design systems, stay updated with our expert insights.'} buttonText={'View all cars'} buttonUrl={''} posts={data}/>
     </div>
-  );
-};
+  )
+}
 
-export default ProductPage;
+export default ProductPage
