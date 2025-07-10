8883b481cf0ee35be0079948bd6d852b23175bc4
ChannimEY <channimey1@gmail.com>
2025-07-10 16:33:58 +0700

    test metadata

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,9 +1,31 @@
 
 import { Suspense } from "react"
 import "../globals.css"
+import type { Metadata } from "next";
 
 import StyledComponentsRegistry from "@/lib/registry"
 import BlogListSkeleton from "@/components/Skeleton/BlogSkeleton"
+export const metadata: Metadata = {
+
+   title:{
+      template:'%s | Car Selling',
+      default: "BlogPage"
+
+   },
+  openGraph:{
+    title:'Car Selling',
+    description:'This is blog page of car selling',
+    images:[
+     {
+      url:'https://leasecar.uk/images/main/cars/2/2/102123/corsa-front.jpeg',
+      width: 800,
+      height: 600,
+      alt: "Car selling platform",
+      type: 'image/png'
+     }
+    ]
+  }
+};
 
 export default function BlogLayout(
     {children}:{children: React.ReactNode}
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,9 +1,30 @@
 
 import DisplayProductComponent from '@/components/products/DisplayProductComponent'
 import FetchCar from '@/lib/api'
+import type { Metadata } from "next";
 import { CarData } from '@/lib/car-type'
 import React from 'react'
+export const metadata: Metadata = {
 
+   title:{
+      template:'%s | Car Selling',
+      default: "productPage"
+
+   },
+  openGraph:{
+    title:'Car Selling',
+    description:'This is product page of car selling',
+    images:[
+     {
+      url:'https://leasecar.uk/images/main/cars/2/2/102123/corsa-front.jpeg',
+      width: 800,
+      height: 600,
+      alt: "Car selling platform",
+      type: 'image/png'
+     }
+    ]
+  }
+};
 const ProductPage = async () => {
   const  data:CarData[] = await FetchCar(0,5)
   console.log(data)
